6b89ed235d38265e2911ef6b770a53205ba83e92
Taqtile <taqtile@Erdinger.local>
2019-05-03 12:11:15 -0300

    craiçao do my first app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Hello from './components/Hello.tsx';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+  //<Hello name="TypeScript" enthusiasmLevel={10} />,
+  //document.getElementById('root') 
 );
